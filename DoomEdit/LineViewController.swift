d58bbf760d17a2813a5c2c4b06b533619995cf36
xiphiasnonus <foster.pianist@gmail.com>
2017-12-11 13:56:44 -0500

    Line panel UI

---
+++ DoomEdit/LineViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LineViewController.swift
+//  DoomEdit
+//
+//  Created by Thomas Foster on 12/10/17.
+//  Copyright © 2017 Thomas Foster. All rights reserved.
+//
+
+import Cocoa
+
+class LineViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
